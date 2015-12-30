87fd901910ff9ebe5bc132634d0dfdb697686e19
Simon Ng <simon@Simons-MacBook-Pro.local>
2015-12-30 23:08:57 +0800

    Initial version

--- LISignIn/ViewController.swift
+++ LISignIn/ViewController.swift
@@ -10,16 +10,103 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlet Properties
+    
+    @IBOutlet weak var btnSignIn: UIButton!
+    
+    @IBOutlet weak var btnGetProfileInfo: UIButton!
+    
+    @IBOutlet weak var btnOpenProfile: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        btnSignIn.enabled = true
+        btnGetProfileInfo.enabled = false
+        btnOpenProfile.hidden = true
     }
 
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        checkForExistingAccessToken()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: IBAction Functions
 
+    @IBAction func getProfileInfo(sender: AnyObject) {
+        if let accessToken = NSUserDefaults.standardUserDefaults().objectForKey("LIAccessToken") {
+            // Specify the URL string that we'll get the profile info from.
+            let targetURLString = "https://api.linkedin.com/v1/people/~:(public-profile-url)?format=json"
+            
+            
+            // Initialize a mutable URL request object.
+            let request = NSMutableURLRequest(URL: NSURL(string: targetURLString)!)
+            
+            // Indicate that this is a GET request.
+            request.HTTPMethod = "GET"
+            
+            // Add the access token as an HTTP header field.
+            request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+            
+            
+            // Initialize a NSURLSession object.
+            let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+            
+            // Make the request.
+            let task: NSURLSessionDataTask = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+                // Get the HTTP status code of the request.
+                let statusCode = (response as! NSHTTPURLResponse).statusCode
+                
+                if statusCode == 200 {
+                    // Convert the received JSON data into a dictionary.
+                    do {
+                        let dataDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
+                        
+                        let profileURLString = dataDictionary["publicProfileUrl"] as! String
+                        
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.btnOpenProfile.setTitle(profileURLString, forState: UIControlState.Normal)
+                            self.btnOpenProfile.hidden = false
+                            
+                        })
+                    }
+                    catch {
+                        print("Could not convert JSON data into a dictionary.")
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    
+    @IBAction func openProfileInSafari(sender: AnyObject) {
+        let profileURL = NSURL(string: btnOpenProfile.titleForState(UIControlState.Normal)!)
+        UIApplication.sharedApplication().openURL(profileURL!)
+    }
+ 
+    
+    // MARK: Custom Functions
+    
+    func checkForExistingAccessToken() {
+        if NSUserDefaults.standardUserDefaults().objectForKey("LIAccessToken") != nil {
+            btnSignIn.enabled = false
+            btnGetProfileInfo.enabled = true
+        }
+    }
+    
 }
 
---
+++ LISignIn/WebViewController.swift
@@ -0,0 +1,177 @@
+//
+//  WebViewController.swift
+//  LISignIn
+//
+//  Created by Gabriel Theodoropoulos on 21/12/15.
+//  Copyright © 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController, UIWebViewDelegate {
+
+    // MARK: IBOutlet Properties
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    // MARK: Constants
+    
+    let linkedInKey = "CLIENT_ID"
+    
+    let linkedInSecret = "CLIENT_SECRET"
+    
+    let authorizationEndPoint = "https://www.linkedin.com/uas/oauth2/authorization"
+    
+    let accessTokenEndPoint = "https://www.linkedin.com/uas/oauth2/accessToken"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        webView.delegate = self
+        
+        startAuthorization()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    // MARK: IBAction Function
+    
+    
+    @IBAction func dismiss(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+ 
+    
+    // MARK: Custom Functions
+    
+    func startAuthorization() {
+        // Specify the response type which should always be "code".
+        let responseType = "code"
+        
+        // Set the redirect URL. Adding the percent escape characthers is necessary.
+        let redirectURL = "https://com.appcoda.linkedin.oauth/oauth".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet())!
+        
+        // Create a random string based on the time intervale (it will be in the form linkedin12345679).
+        let state = "linkedin\(Int(NSDate().timeIntervalSince1970))"
+        
+        // Set preferred scope.
+        let scope = "r_basicprofile"
+        
+        
+        // Create the authorization URL string.
+        var authorizationURL = "\(authorizationEndPoint)?"
+        authorizationURL += "response_type=\(responseType)&"
+        authorizationURL += "client_id=\(linkedInKey)&"
+        authorizationURL += "redirect_uri=\(redirectURL)&"
+        authorizationURL += "state=\(state)&"
+        authorizationURL += "scope=\(scope)"
+        
+        print(authorizationURL)
+        
+        
+        // Create a URL request and load it in the web view.
+        let request = NSURLRequest(URL: NSURL(string: authorizationURL)!)
+        webView.loadRequest(request)
+    }
+    
+    
+    func requestForAccessToken(authorizationCode: String) {
+        let grantType = "authorization_code"
+        
+        let redirectURL = "https://com.appcoda.linkedin.oauth/oauth".stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet())!
+        
+        // Set the POST parameters.
+        var postParams = "grant_type=\(grantType)&"
+        postParams += "code=\(authorizationCode)&"
+        postParams += "redirect_uri=\(redirectURL)&"
+        postParams += "client_id=\(linkedInKey)&"
+        postParams += "client_secret=\(linkedInSecret)"
+        
+        // Convert the POST parameters into a NSData object.
+        let postData = postParams.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        
+        // Initialize a mutable URL request object using the access token endpoint URL string.
+        let request = NSMutableURLRequest(URL: NSURL(string: accessTokenEndPoint)!)
+        
+        // Indicate that we're about to make a POST request.
+        request.HTTPMethod = "POST"
+        
+        // Set the HTTP body using the postData object created above.
+        request.HTTPBody = postData
+        
+        // Add the required HTTP header field.
+        request.addValue("application/x-www-form-urlencoded;", forHTTPHeaderField: "Content-Type")
+        
+        
+        // Initialize a NSURLSession object.
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        
+        // Make the request.
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            // Get the HTTP status code of the request.
+            let statusCode = (response as! NSHTTPURLResponse).statusCode
+            
+            if statusCode == 200 {
+                // Convert the received JSON data into a dictionary.
+                do {
+                    let dataDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
+                    
+                    let accessToken = dataDictionary["access_token"] as! String
+                    
+                    NSUserDefaults.standardUserDefaults().setObject(accessToken, forKey: "LIAccessToken")
+                    NSUserDefaults.standardUserDefaults().synchronize()
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                    })
+                }
+                catch {
+                    print("Could not convert JSON data into a dictionary.")
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
+    // MARK: UIWebViewDelegate Functions
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        let url = request.URL!
+        print(url)
+        
+        if url.host == "com.appcoda.linkedin.oauth" {
+            if url.absoluteString.rangeOfString("code") != nil {
+                // Extract the authorization code.
+                let urlParts = url.absoluteString.componentsSeparatedByString("?")
+                let code = urlParts[1].componentsSeparatedByString("=")[1]
+                
+                requestForAccessToken(code)
+            }
+        }
+        
+        return true
+    }
+    
+}
